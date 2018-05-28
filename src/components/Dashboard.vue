8651fe29c9c6561138a2941d290e58849c82a4ff
Ryosuke Kawamura <clonable.eden@gmail.com>
2018-05-28 21:50:44 +0900

    rename Dashboard & delete spec once

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'DashBoard',
+  name: 'Dashboard',
 };
 </script>
 
--- test/unit/specs/DashBoard.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import DashBoard from '@/components/DashBoard';
-
-describe('DashBoard.vue', () => {
-  it('should render correct contents', () => {
-    const vm = mount(DashBoard);
-    expect(vm.is(DashBoard)).toBe(true);
-  });
-});
